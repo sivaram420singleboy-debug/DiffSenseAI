9f66f06bb6aa468fd5f362d79a6d42e5d9f8de36
Sivaram <your-email@gmail.com>
2026-03-30 14:08:17 +0530

    db created

--- server_api/utils/db_helper.py
+++ server_api/utils/db_helper.py
@@ -1,6 +1,18 @@
 import sqlite3
+import os
 
-DB_PATH = "database/db.sqlite3"
+def get_db_path():
+    # 🔥 Render path
+    if os.environ.get("RENDER"):
+        return "/opt/render/project/src/server_api/database/db.sqlite3"
+    else:
+        # 💻 Local path
+        return "server_api/database/db.sqlite3"
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
+    path = get_db_path()
+
+    print("🔥 DB PATH:", path)
+
+    conn = sqlite3.connect(path)
+    return conn
