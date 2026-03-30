9f0e762f82e3e9a6ae5e06b584ccee656d738a3f
Sivaram <your-email@gmail.com>
2026-03-30 15:10:21 +0530

    fixed postgres

--- create_db.py
+++ create_db.py
@@ -1,28 +1,16 @@
-import sqlite3
-import os
+def get_connection():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASS"),
+            port=os.environ.get("DB_PORT")
+        )
 
-db_path = "server_api/database/db.sqlite3"
+        print("🔥 PostgreSQL Connected Successfully")
+        return conn
 
-os.makedirs(os.path.dirname(db_path), exist_ok=True)
-
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS Licenses(
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    LicenseKey TEXT UNIQUE,
-    MachineId TEXT,
-    IsUsed INTEGER DEFAULT 0,
-    ExpiryDate TEXT
-)
-""")
-
-# 🔥 ADD TEST KEYS
-cur.execute("INSERT OR IGNORE INTO Licenses (LicenseKey, IsUsed) VALUES ('LIC-ABC-12345', 0)")
-cur.execute("INSERT OR IGNORE INTO Licenses (LicenseKey, IsUsed) VALUES ('LIC-TEST-11111', 0)")
-
-conn.commit()
-conn.close()
-
-print("✅ DB created correctly")
+    except Exception as e:
+        print("❌ DB CONNECTION ERROR:", e)
+        raise e   # 🔥 CRASH HERE DIRECTLY (GOOD)
